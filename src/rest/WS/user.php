be204d4689d9feb202f4a8b1187ee17537df851a
femathieu <mathieuferron06@gmail.com>
2019-03-04 16:48:53 +0100

    setup monologue for log handling

--- src/rest/WS/user.php
+++ src/rest/WS/user.php
@@ -4,8 +4,8 @@ use Slim\Http\Request;
 
 use \Controller\UserController;
 
-$app->get('/test', function(Request $request, Response $response, array $args){
+$app->get('/user/list', function(Request $request, Response $response, array $args){
+    $this->logger->addInfo('ws -> test');
     $ctrl = new UserController();
-    // echo json_encode(array("test" => "test"));
     echo json_encode($ctrl->getUserList());
 });
--- src/rest/index.php
+++ src/rest/index.php
@@ -8,22 +8,14 @@ require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/log_writer.php');
 require_once('../config/config.php');
 
-$cutPath = explode('/src', __DIR__);
-$path = $cutPath[0] . '/logs/server_error.log';
-
 //set log path
 ini_set('error_log', $path); 
 
 // Logging file size
 ini_set('log_errors_max_len', 2048); 
 
-error_reporting(-1);
-ini_set('display_errors', 1);
- 
-// setup log writer
-$logWriter = new LogWriter();
-$logWriter->setFile($path);
-$logWriter->openFile();
+// error_reporting(-1);
+// ini_set('display_errors', 1);
 
 spl_autoload_register(function($className){
     $file = __DIR__ . '/class/' . str_replace('\\', '/', $className) . '.class.php';
@@ -34,9 +26,19 @@ spl_autoload_register(function($className){
 
 $app = new \Slim\App($config);
 
+$container = $app->getContainer();
+$container['logger'] = function($c) {
+    $cutPathLog = explode('/src', __DIR__);
+    $pathLog = $cutPathLog[0] . '/logs/server_error.log';
+    $logger = new \Monolog\Logger('app_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler($pathLog);
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$app->getContainer()->logger->addInfo('start session');
+
 require_once(__DIR__ . '/WS/user.php');
 
 $app->run();
 
-// close log file after finishing app
-$logWriter->closeFile();
