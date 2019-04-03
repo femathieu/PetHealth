3cd109229c398eaaaefef03695bed7ff570b3086
camiller00 <camille.raher@atalanta.fr>
2019-04-03 21:14:24 +0200

    update scss on header overview bg

---
+++ src/webapp/src/app/header-overview/header-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-overview',
+  templateUrl: './header-overview.component.html',
+  styleUrls: ['./header-overview.component.scss']
+})
+export class HeaderOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
