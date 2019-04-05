c14a777f7bf4aa5c9133c28e787fb82ffaf43f31
mathieuf <mathieuferron06@gmail.com>
2019-04-05 23:28:05 +0200

    container database : config for windows

--- container/database/start.sh
+++ container/database/start.sh
@@ -4,9 +4,18 @@ MYSQL="/usr/bin/mysql"
 
 service mysql stop
 usermod -d /var/lib/mysql/ mysql
+
+mkdir /var/run/mysqld
+touch /var/run/mysqld/mysqld.sock
+chown -R mysql /var/run/mysqld
+
+apt-get clean
+apt-get -f remove
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+rm /var/lib/mysql/ib_logfile*
 service mysql start
 
-mysql -e "source /db.sql;";
+mysql -e "source /db.sql;"
 mysql --user=web --password=capima -e "source /pethealth.sql"
 
 tail -f /start.sh
