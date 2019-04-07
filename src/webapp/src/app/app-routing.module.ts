d90dfd36512d059972f6973ba934e25751990339
camiller00 <camille.raher@atalanta.fr>
2019-04-07 16:33:50 +0200

    edit routing from create-pet

--- src/webapp/src/app/app-routing.module.ts
+++ src/webapp/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { CreatePetComponent } from './create-pet/create-pet.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'create-pet', component: CreatePetComponent },
 ];
 
 @NgModule({
